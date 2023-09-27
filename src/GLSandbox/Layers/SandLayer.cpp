210abf5e26d468bf35af478e374caf9541f3662f
Patrik <papsop@outlook.com>
2023-09-27 10:36:17 +0200

    Custom texture rendering init

--- src/GLSandbox/Layers/SandLayer.cpp
+++ src/GLSandbox/Layers/SandLayer.cpp
@@ -1,11 +1,44 @@
 #include <GLSandbox/Layers/SandLayer.h>
 #include <GLCore/Core/Application.h>
 
+#include <GLCore/Core/GLFWGlad.h>
+
 namespace GLSandbox {
 
 void SandLayer::OnAttach()
 {
   REGISTER_EVENT_CALLBACK(GLCore::E_SandboxCanvasMouseEvent, this, &SandLayer::OnSandboxCanvasMouseEvent);
+
+  m_textureShader.LoadShadersFromFiles("../assets/shaders/texShader.vert.glsl", "../assets/shaders/texShader.frag.glsl");
+
+  // TEXTURE
+  glGenTextures(1, &m_textureID);
+  glBindTexture(GL_TEXTURE_2D, m_textureID);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);  // size increase/decrease
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);  // WRAPPING
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, m_pixelsWidth, m_pixelsHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, m_pixelsBuffer);
+  glBindTexture(GL_TEXTURE_2D, 0);
+
+  // QUAD
+  float quadVertices[] = {// vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates.
+                          // positions   // texCoords
+                          -1.0f, 1.0f, 0.0f, 1.0f, -1.0f, -1.0f, 0.0f, 0.0f, 1.0f, -1.0f, 1.0f, 0.0f,
+
+                          -1.0f, 1.0f, 0.0f, 1.0f, 1.0f,  -1.0f, 1.0f, 0.0f, 1.0f, 1.0f,  1.0f, 1.0f};
+  glGenVertexArrays(1, &m_quadVAO);
+  glGenBuffers(1, &m_quadVBO);
+  glBindVertexArray(m_quadVAO);
+  glBindBuffer(GL_ARRAY_BUFFER, m_quadVBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(1);
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindVertexArray(0);
 }
 
 void SandLayer::OnDetach()
@@ -15,6 +48,28 @@ void SandLayer::OnDetach()
 
 void SandLayer::OnUpdate(GLCore::Timestep dt)
 {
+  unsigned char* nextPixel = &m_pixelsBuffer[0];
+  for (int i = 0; i < m_pixelsWidth * m_pixelsHeight; i++) {
+    const unsigned char channel = static_cast<unsigned char>(255.0);
+    *nextPixel = 125;
+    ++nextPixel;
+    *nextPixel = 125;
+    ++nextPixel;
+    *nextPixel = 125;
+    ++nextPixel;
+    *nextPixel = 255;
+    ++nextPixel;
+  }
+
+  glBindTexture(GL_TEXTURE_2D, m_textureID);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, m_pixelsWidth, m_pixelsHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, m_pixelsBuffer);
+  glBindTexture(GL_TEXTURE_2D, 0);
+
+  m_textureShader.Use();
+  glBindVertexArray(m_quadVAO);
+  glBindTexture(GL_TEXTURE_2D, m_textureID);
+  glDrawArrays(GL_TRIANGLES, 0, 6);
+  glBindVertexArray(0);
 }
 
 void SandLayer::OnSandboxCanvasMouseEvent(const GLCore::E_SandboxCanvasMouseEvent& e)
--- src/GLSandbox/Layers/SandLayer.h
+++ src/GLSandbox/Layers/SandLayer.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <GLCore/Core/Layer.h>
 #include <GLCore/Core/Events.h>
+#include <GLCore/Utils/Shader.h>
 
 namespace GLSandbox {
 class SandLayer : public GLCore::I_Layer {
@@ -16,5 +17,14 @@ class SandLayer : public GLCore::I_Layer {
   void OnSandboxCanvasMouseEvent(const GLCore::E_SandboxCanvasMouseEvent& e);
 
  private:
+  static constexpr size_t m_pixelsWidth = 400;
+  static constexpr size_t m_pixelsHeight = 400;
+  unsigned char m_pixelsBuffer[m_pixelsWidth * m_pixelsHeight * 4];
+
+  GLCore::Shader m_textureShader;
+
+  unsigned int m_textureID;
+  unsigned int m_quadVAO;
+  unsigned int m_quadVBO;
 };
 }  // namespace GLSandbox
